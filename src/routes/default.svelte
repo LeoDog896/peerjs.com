2dcc1c289cd5ecbf6daab34efcb35bf63f78a7dd
Tristan <LeoDog896@hotmail.com>
2022-04-29 23:34:13 -0400

    Begin sveltekit move

---
+++ src/routes/default.svelte
@@ -0,0 +1,31 @@
+<script>
+	import '../app.css';
+	import '../button.css';
+	import '../lato.css';
+	import '../style.css';
+	import 'github-fork-ribbon-css/gh-fork-ribbon.css';
+</script>
+
+<a class="github-fork-ribbon" href="https://github.com/peers" data-ribbon="Fork PeerJS on GitHub" title="Fork PeerJS on GitHub">Fork PeerJS on GitHub</a>
+<div id="container">
+	<div id="nav-wrap">
+		<div class="sign">
+			PEER
+			<span class="highlight">JS</span>
+		</div>
+		<div id="nav">
+			<a href="/">Home</a>
+			<a href="/docs#start">Get Started</a>
+			<a href="/docs#api">API Reference</a>
+			<a href="/examples">Examples</a>
+			<a href="/peerserver">PeerServer Cloud</a>
+			<a href="https://stats.uptimerobot.com/B7Ww3sDBVz" target="_blank">Status page</a>
+			<a href="/team">Team</a>
+		</div>
+	</div>
+	<div id="content">
+		<div id="text">
+			<slot></slot>
+		</div>
+	</div>
+</div>
---
+++ src/routes/peerserver.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Default from "./default.svelte"
+  
+  const gratitude = false;
+</script>
+
+<Default>
+  <h2>PeerServer Cloud service</h2>
+  <p>If you DON'T specify 'host' and 'key' options, you will automatically connect to PeerServer Cloud service. Please be aware
+    that you will be sharing it with other people and IDs may collide if you set them manually.
+    <br/>
+    Problems? View our <a href="https://stats.uptimerobot.com/B7Ww3sDBVz" target="_blank">status page!</a></p>
+
+
+  {#if gratitude}
+    <p class="tagline">Thanks for your support! :)</p>
+  {:else}
+    <h3>Help us keep our servers running :)</h3>
+    <button on:click={() => window.open('https://opencollective.com/peer', '_blank')} class="download">Donate!</button>
+  {/if}
+</Default>
